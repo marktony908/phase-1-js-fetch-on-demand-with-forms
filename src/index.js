c095b3155d2fa6cee957beece344bdf77c0b5d2f
Tony <kipkoechtony510@gmail.com>
2024-07-13 13:00:18 +0300

    Initial commit of movie database assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+    document.addEventListener("DOMContentLoaded", init)
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit",(Event) => {
+        Event.preventDefault();
+        const input =document.querySelector("input#searchByID");
+
+fetch(`http://localhost:3000/movies/1${input.Value}`)
+.then((Response) => Response.json())
+.then((data) => {
+    const title = document.querySelector("section#movieDetails h4");
+    const summary = document.querySelector("section#movieDetails p");
+
+    title.innerText = data.title;
+        summary.innerText = data.summary;
+
+});
+    });
+};
   
-}
+
+
 
 document.addEventListener('DOMContentLoaded', init);
